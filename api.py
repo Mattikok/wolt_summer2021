92d7a478f983e7f69338a32abea86e44eecf7b48
Mattikok <mattik1999@gmail.com>
2021-01-19 17:25:58 +0200

    added check for proper latitude and longitude parameters

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ import json  # json to load the data file to memory
 from flask import abort, jsonify, request  # imports to use
 import restaurants as rt  # import helper class
 
-app = flask.Flask('WoltSummerJob')
-app.config['DEBUG'] = True  # set to debug mode
+app = flask.Flask('WoltSummerJob')  # set name to be package name for easier debugging
+app.config['DEBUG'] = True  # set to debug mode for better developing
 app.config['JSON_SORT_KEYS'] = False  # set to not sort dictionarys with jsonify
 # since it would mess up the order defined in restaurants.py
 
@@ -18,8 +18,12 @@ with open('restaurants.json', 'r') as file:  # use the data from the file,
 @app.route('/discovery', methods=['GET'])
 def discovery():
     if 'lat' and 'lon' in request.args:  # check that latitude and longitude are given
-        latitude = float(request.args['lat'])
-        longitude = float(request.args['lon'])
+        try:
+            latitude = float(request.args['lat'])
+            longitude = float(request.args['lon'])
+        except ValueError:
+            abort(400, description='Check latitude and longitude')
+            return
     else:  # abort if not specified
         abort(404, description="Missing arguments lat or lon")
         return
